2c9eb9b1cb5c43267dbe6ef2f0fcf2d6442bffa1
Igor <igorhenss@gmail.com>
2019-07-11 22:15:54 -0300

    Interface PersonagemRepository finalizada.

--- src/main/java/com/igorhenss/killNdice/killNdice/repository/PersonagemRepository.java
+++ src/main/java/com/igorhenss/killNdice/killNdice/repository/PersonagemRepository.java
@@ -1,10 +1,20 @@
 package com.igorhenss.killNdice.killNdice.repository;
 
 import com.igorhenss.killNdice.killNdice.domain.entity.Personagem;
+import com.igorhenss.killNdice.killNdice.enumerated.Alinhamento;
+import com.igorhenss.killNdice.killNdice.enumerated.Classe;
+import com.igorhenss.killNdice.killNdice.enumerated.Profissao;
+import com.igorhenss.killNdice.killNdice.enumerated.Raca;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface PersonagemRepository extends JpaRepository<Personagem, Long> {
 
-    
+    Personagem getByName(String name);
+    List<Personagem> getByAlinhamento(Alinhamento alinhamento);
+    List<Personagem> getByClasse(Classe classe);
+    List<Personagem> getByProfissao(Profissao profissao);
+    List<Personagem> getByRaca(Raca raca);
 
 }
