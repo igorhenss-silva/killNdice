a197cfdd9dc71becbefb2176955247fba6e8065d
Igor <igorhenss@gmail.com>
2019-07-11 22:02:13 -0300

    Métodos Getters and Setters adicionados.

--- src/main/java/com/igorhenss/killNdice/killNdice/domain/dto/PersonagemDTO.java
+++ src/main/java/com/igorhenss/killNdice/killNdice/domain/dto/PersonagemDTO.java
@@ -53,4 +53,62 @@ public class PersonagemDTO implements Serializable {
         return Objects.hash(id, nome);
     }
 
+    // GETTERS AND SETTERS
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getHitpoints() {
+        return hitpoints;
+    }
+
+    public void setHitpoints(Integer hitpoints) {
+        this.hitpoints = hitpoints;
+    }
+
+    public Alinhamento getAlinhamento() {
+        return alinhamento;
+    }
+
+    public void setAlinhamento(Alinhamento alinhamento) {
+        this.alinhamento = alinhamento;
+    }
+
+    public Classe getClasse() {
+        return classe;
+    }
+
+    public void setClasse(Classe classe) {
+        this.classe = classe;
+    }
+
+    public Profissao getProfissao() {
+        return profissao;
+    }
+
+    public void setProfissao(Profissao profissao) {
+        this.profissao = profissao;
+    }
+
+    public Raca getRaca() {
+        return raca;
+    }
+
+    public void setRaca(Raca raca) {
+        this.raca = raca;
+    }
+
 }
