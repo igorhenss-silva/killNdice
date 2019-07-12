2996b3dd09bda38bb32a848dfe51a2a40b85311b
Igor <igorhenss@gmail.com>
2019-07-11 21:33:55 -0300

    Atributos da classe Personagem adicionados.

--- src/main/java/com/igorhenss/killNdice/killNdice/domain/entity/Personagem.java
+++ src/main/java/com/igorhenss/killNdice/killNdice/domain/entity/Personagem.java
@@ -1,7 +1,15 @@
 package com.igorhenss.killNdice.killNdice.domain.entity;
 
+import com.igorhenss.killNdice.killNdice.enumerated.*;
+
 public class Personagem {
 
-    
+    private String nome;
+    private Integer hitpoints;
+    private Alinhamento alinhamento;
+    private Atributo atributo;
+    private Classe classe;
+    private Profissao profissao;
+    private Raca raca;
 
 }
