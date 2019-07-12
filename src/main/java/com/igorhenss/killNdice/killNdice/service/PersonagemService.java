5069c75d5d273ca791df7d9e8f1ecc89de0146c0
Igor <igorhenss@gmail.com>
2019-07-11 22:17:23 -0300

    @Autowired PersonagemRepository adicionado ao @Service PersonagemService.

--- src/main/java/com/igorhenss/killNdice/killNdice/service/PersonagemService.java
+++ src/main/java/com/igorhenss/killNdice/killNdice/service/PersonagemService.java
@@ -1,10 +1,13 @@
 package com.igorhenss.killNdice.killNdice.service;
 
+import com.igorhenss.killNdice.killNdice.repository.PersonagemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PersonagemService {
 
-    
+    @Autowired
+    private PersonagemRepository personagemRepository;
 
 }
