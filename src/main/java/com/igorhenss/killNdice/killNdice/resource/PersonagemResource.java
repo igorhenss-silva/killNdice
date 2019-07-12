4c348b4461b19e0363d586508267b8e60d4bf001
Igor <igorhenss@gmail.com>
2019-07-11 23:40:14 -0300

    @Autowired PersonagemService e @PostMapping salvar adicionados ao @RestController PersonagemResource.

--- src/main/java/com/igorhenss/killNdice/killNdice/resource/PersonagemResource.java
+++ src/main/java/com/igorhenss/killNdice/killNdice/resource/PersonagemResource.java
@@ -1,5 +1,10 @@
 package com.igorhenss.killNdice.killNdice.resource;
 
+import com.igorhenss.killNdice.killNdice.domain.dto.PersonagemDTO;
+import com.igorhenss.killNdice.killNdice.service.PersonagemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/personagens")
 public class PersonagemResource {
 
-    
+    @Autowired
+    private PersonagemService personagemService;
+
+    @PostMapping
+    public PersonagemDTO salvar(@RequestBody PersonagemDTO body) {
+        return personagemService.salvar(body);
+    }
 
 }
