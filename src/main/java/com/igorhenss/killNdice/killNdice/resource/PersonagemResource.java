ad2515acff5ce3526aa71baa9fee03765d357ca1
Igor <igorhenss@gmail.com>
2019-07-11 23:42:16 -0300

    @GetMapping getByNomeParcial adicionado ao @RestController PersonagemResource.

--- src/main/java/com/igorhenss/killNdice/killNdice/resource/PersonagemResource.java
+++ src/main/java/com/igorhenss/killNdice/killNdice/resource/PersonagemResource.java
@@ -3,10 +3,9 @@ package com.igorhenss.killNdice.killNdice.resource;
 import com.igorhenss.killNdice.killNdice.domain.dto.PersonagemDTO;
 import com.igorhenss.killNdice.killNdice.service.PersonagemService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/personagens")
@@ -18,6 +17,11 @@ public class PersonagemResource {
     @PostMapping
     public PersonagemDTO salvar(@RequestBody PersonagemDTO body) {
         return personagemService.salvar(body);
+    }
+
+    @GetMapping(value = "/nomeParcial/{nome_parcial}")
+    public List<PersonagemDTO> getByNomeParcial(@PathVariable("nome_parcial") String nome) {
+        return personagemService.getByNomeParcial(nome);
     }
 
 }
