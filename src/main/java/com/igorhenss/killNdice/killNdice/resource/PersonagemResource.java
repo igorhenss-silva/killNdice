c8cd9d26f48447e973dd82a3288f7c9c2afdd0a0
Igor <igorhenss@gmail.com>
2019-07-11 23:18:48 -0300

    @RestController e @RequestMapping adicionados.

--- src/main/java/com/igorhenss/killNdice/killNdice/resource/PersonagemResource.java
+++ src/main/java/com/igorhenss/killNdice/killNdice/resource/PersonagemResource.java
@@ -1,4 +1,12 @@
 package com.igorhenss.killNdice.killNdice.resource;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/personagens")
 public class PersonagemResource {
+
+    
+
 }
