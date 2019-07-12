4494f8f95a238a837f893c0a5ba457f76fc6218c
Igor <igorhenss@gmail.com>
2019-07-11 22:04:51 -0300

    Interface PersonagemRepository criada.

---
+++ src/main/java/com/igorhenss/killNdice/killNdice/repository/PersonagemRepository.java
@@ -0,0 +1,10 @@
+package com.igorhenss.killNdice.killNdice.repository;
+
+import com.igorhenss.killNdice.killNdice.domain.entity.Personagem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonagemRepository extends JpaRepository<Personagem, Long> {
+
+    
+
+}
