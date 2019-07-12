1b86ef9467271b408cd9475e42d9569ff635b292
Igor <igorhenss@gmail.com>
2019-07-11 21:57:40 -0300

    Atributos adicionados à classe PersonagemDTO.

--- src/main/java/com/igorhenss/killNdice/killNdice/domain/dto/PersonagemDTO.java
+++ src/main/java/com/igorhenss/killNdice/killNdice/domain/dto/PersonagemDTO.java
@@ -1,9 +1,22 @@
 package com.igorhenss.killNdice.killNdice.domain.dto;
 
+import com.igorhenss.killNdice.killNdice.enumerated.Alinhamento;
+import com.igorhenss.killNdice.killNdice.enumerated.Classe;
+import com.igorhenss.killNdice.killNdice.enumerated.Profissao;
+import com.igorhenss.killNdice.killNdice.enumerated.Raca;
+
 import java.io.Serializable;
 
 public class PersonagemDTO implements Serializable {
 
-    
+    public static final long serialVersionUID = 23L;
+
+    private Long id;
+    private String nome;
+    private Integer hitpoints;
+    private Alinhamento alinhamento;
+    private Classe classe;
+    private Profissao profissao;
+    private Raca raca;
 
 }
