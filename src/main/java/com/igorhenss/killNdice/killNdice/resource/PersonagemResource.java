0538a7d1f5c70012fe23ae7fd754799bbe099873
Igor <igorhenss@gmail.com>
2019-07-11 23:44:17 -0300

    @GetMapping getByAlinhamento adicionado ao @RestController PersonagemResource.

--- src/main/java/com/igorhenss/killNdice/killNdice/resource/PersonagemResource.java
+++ src/main/java/com/igorhenss/killNdice/killNdice/resource/PersonagemResource.java
@@ -1,6 +1,7 @@
 package com.igorhenss.killNdice.killNdice.resource;
 
 import com.igorhenss.killNdice.killNdice.domain.dto.PersonagemDTO;
+import com.igorhenss.killNdice.killNdice.enumerated.Alinhamento;
 import com.igorhenss.killNdice.killNdice.service.PersonagemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -27,6 +28,11 @@ public class PersonagemResource {
     @GetMapping(value = "/nome/{nome}")
     public PersonagemDTO getByNome(@PathVariable("nome") String nome) {
         return personagemService.getByNome(nome);
+    }
+
+    @GetMapping(value = "/alinhamento/{alinhamento}")
+    public List<PersonagemDTO> getByAlinhamento(@PathVariable("alinhamento") Alinhamento alinhamento) {
+        return personagemService.getByAlinhamento(alinhamento);
     }
 
 }
