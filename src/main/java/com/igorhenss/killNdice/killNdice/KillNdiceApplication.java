cffbb65a2711950fde0136188384280b5683e50e
Igor <igorhenss@gmail.com>
2019-07-11 21:07:51 -0300

    Projeto killNdice criado.

---
+++ src/main/java/com/igorhenss/killNdice/killNdice/KillNdiceApplication.java
@@ -0,0 +1,13 @@
+package com.igorhenss.killNdice.killNdice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KillNdiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KillNdiceApplication.class, args);
+	}
+
+}
