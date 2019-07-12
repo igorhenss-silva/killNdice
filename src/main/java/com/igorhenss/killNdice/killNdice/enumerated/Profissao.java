d1232122fa22cb4a861ef086700f20add0a0b699
Igor <igorhenss@gmail.com>
2019-07-11 21:23:25 -0300

    Todas as profissões foram adicionadas.

--- src/main/java/com/igorhenss/killNdice/killNdice/enumerated/Profissao.java
+++ src/main/java/com/igorhenss/killNdice/killNdice/enumerated/Profissao.java
@@ -2,6 +2,7 @@ package com.igorhenss.killNdice.killNdice.enumerated;
 
 public enum Profissao {
 
-    
+    ALFAIATE, CONSTRUTOR, ENCANTADOR, ENGENHEIRO, FERREIRO,
+    HERBALISTA, MERCENARIO, MINEIRO, QUIMICO
 
 }
