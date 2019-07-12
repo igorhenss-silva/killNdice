f93a7f32b2f686b5b1eb096bfb3d359f109026e8
Igor <igorhenss@gmail.com>
2019-07-11 22:30:33 -0300

    Método save adicionado à classe PersonagemService.

--- src/main/java/com/igorhenss/killNdice/killNdice/service/PersonagemService.java
+++ src/main/java/com/igorhenss/killNdice/killNdice/service/PersonagemService.java
@@ -1,5 +1,8 @@
 package com.igorhenss.killNdice.killNdice.service;
 
+import com.igorhenss.killNdice.killNdice.domain.dto.PersonagemDTO;
+import com.igorhenss.killNdice.killNdice.domain.entity.Personagem;
+import com.igorhenss.killNdice.killNdice.infrastructure.CriadorDePersonagem;
 import com.igorhenss.killNdice.killNdice.repository.PersonagemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,5 +12,28 @@ public class PersonagemService {
 
     @Autowired
     private PersonagemRepository personagemRepository;
+
+    // CREATE
+
+    public PersonagemDTO save(PersonagemDTO dto) {
+        Personagem personagem = new CriadorDePersonagem().comNomeIgualA(dto.getNome())
+                                                        .comHitpointsMaximosIgualA(dto.getHitpointsMaximos())
+                                                        .comHitpointsIgualA(dto.getHitpoints())
+                                                        .comAlinhamentoIgualA(dto.getAlinhamento())
+                                                        .comClasseIgualA(dto.getClasse())
+                                                        .comProfissaoIgualA(dto.getProfissao())
+                                                        .comRacaIgualA(dto.getRaca())
+                                                        .criar();
+        return personagemToDto(personagemRepository.save(personagem));
+    }
+
+    // METHODS
+
+    private PersonagemDTO personagemToDto(Personagem personagem) {
+        return new PersonagemDTO(personagem.getId(), personagem.getNome(),
+                personagem.getHitpointsMaximos(), personagem.getHitpoints(),
+                personagem.getAlinhamento(), personagem.getClasse(),
+                personagem.getProfissao(), personagem.getRaca());
+    }
 
 }
